63e81f81a46138b83c0878cc7677162f21123a95
R Yusuf Raihan Setiawan <yuraindo8@gmail.com>
2021-09-24 15:00:30 +0700

    meneruskan object-type.php dan menggunakan inheritance untuk memudahkan kita jika ada kategori baru, berdasarkan yang terdapat di video bagian ke 6

---
+++ inheritance-problem.php
@@ -0,0 +1,55 @@
+<?php
+
+// Jualan Produk
+// Komik
+// Game
+class Produk {
+    public  $judul,
+            $penulis, 
+            $penerbit,
+            $harga,
+            $jmlHalaman,
+            $waktumain,
+            $tipe;
+
+    public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0, $waktumain = 0, $tipe) {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+        $this->jmlhalaman = $jmlHalaman;
+        $this->waktuMain = $waktumain;
+        $this->tipe = $tipe;
+    }
+
+    public function getLabel() {
+        return "$this->penulis, $this->penerbit";
+    }
+
+    public function getInfoLengkap() {
+        $str = "{$this->tipe} : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+        if ( $this->tipe == "Komik") {
+            $str .= " - {$this->jmlHalaman} Halaman.";
+        } else if ($this->tipe == "Game") {
+            $str .= " ~ {$this->waktuMain} Jam.";
+        }
+
+        return $str;
+    }
+
+}
+
+class CetakInfoProduk {
+    public function cetak ( Produk $produk ) {
+        $str = "{$produk->judul} | {$produk->getLabel()}, (Rp. {$produk->harga})";
+        return $str;
+    }
+}
+
+
+$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", "30000", 100, 0 , "Komik");
+$produk2 = new Produk("Uncharted", "Neil Druckman", "Sony Computer", "250000", 0, 50, "Game");
+
+echo $produk1->getInfoLengkap();
+
+?>
